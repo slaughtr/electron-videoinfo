a4f39c58350842a3b7687618f3530bf3f17f825e
slaughtr <dslaughtr@gmail.com>
2017-07-24 12:44:24 -0700

    Import ffmpeg fluent, add code

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const electron = require('electron')
-
-const { app, BrowserWindow } = electron
+const { app, BrowserWindow, ipcMain } = electron
+const ffmpeg = require('fluent-ffmpeg')
 
 app.on('ready', () => {
 	const mainWindow = new BrowserWindow({})
 	mainWindow.loadURL(`file://${__dirname}/index.html`)
+})
+
+ipcMain.on('videosubmitted', (event, path) => {
+	ffmpeg.ffprobe(path, (err, metadata) => {
+		console.log('Length: '+metadata.format.duration)
+	})
 })
